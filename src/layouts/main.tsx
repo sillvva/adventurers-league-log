c16b9ef17f1d697987cb1db8019bebb687b7b585
Matt Dekok <matt.dekok@gmail.com>
2022-08-13 19:37:17 -0500

    Loading Bar

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -5,8 +5,9 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/future/image";
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { PropsWithChildren, useState } from "react";
+import Router, { useRouter } from "next/router";
+import * as NProgress from "nprogress";
+import { PropsWithChildren, useEffect, useState } from "react";
 import background from "../../public/images/barovia-gate.jpg";
 import google from "../../public/images/google.svg";
 
@@ -21,6 +22,7 @@ const Layout = (props: PropsWithChildren) => {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </Head>
+			<NextNProgress color="#6518e7" height={2} options={{ showSpinner: false }} />
       <Image
         src={background}
         priority
@@ -159,3 +161,150 @@ const Layout = (props: PropsWithChildren) => {
 };
 
 export default Layout;
+
+type NProgress = {
+	/**
+	 * The color of the bar.
+	 * @default "#29D"
+	 */
+	color?: string;
+	/**
+	 * The start position of the bar.
+	 * @default 0.3
+	 */
+	startPosition?: number;
+	/**
+	 * The stop delay in milliseconds.
+	 * @default 200
+	 */
+	stopDelayMs?: number;
+	/**
+	 * The height of the bar.
+	 * @default 3
+	 */
+	height?: number;
+	/**
+	 * Whether to show the bar on shallow routes.
+	 * @default true
+	 */
+	showOnShallow?: boolean;
+	/**
+	 * The other NProgress configuration options to pass to NProgress.
+	 * @default null
+	 */
+	options?: Partial<NProgress.NProgressOptions>;
+	/**
+	 * The nonce attribute to use for the `style` tag.
+	 * @default undefined
+	 */
+	nonce?: string;
+};
+
+function NextNProgress({ color = "#29D", startPosition = 0.3, stopDelayMs = 200, height = 3, showOnShallow = true, options, nonce }: NProgress) {
+	useEffect(() => {
+		let timer: NodeJS.Timeout | null = null;
+
+		if (options) {
+			NProgress.configure(options);
+		}
+
+		const routeChangeStart = (_: string, { shallow }: { shallow: boolean }) => {
+			if (!shallow || showOnShallow) {
+				NProgress.set(startPosition);
+				NProgress.start();
+			}
+		};
+
+		const routeChangeEnd = (_: string, { shallow }: { shallow: boolean }) => {
+			if (!shallow || showOnShallow) {
+				if (timer) clearTimeout(timer);
+				timer = setTimeout(() => {
+					NProgress.done(true);
+				}, stopDelayMs);
+			}
+		};
+
+		Router.events.on("routeChangeStart", routeChangeStart);
+		Router.events.on("routeChangeComplete", routeChangeEnd);
+		Router.events.on("routeChangeError", routeChangeEnd);
+
+		return () => {
+			if (timer) clearTimeout(timer);
+			NProgress.done(true);
+			Router.events.off("routeChangeStart", routeChangeStart);
+			Router.events.off("routeChangeComplete", routeChangeEnd);
+			Router.events.off("routeChangeError", routeChangeEnd);
+		};
+	}, [options, showOnShallow, startPosition, stopDelayMs]);
+
+	return (
+		<style nonce={nonce}>{`
+      #nprogress {
+        pointer-events: none;
+      }
+      #nprogress .bar {
+        background: ${color};
+        position: fixed;
+        z-index: 9999;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: ${height}px;
+      }
+      #nprogress .peg {
+        display: block;
+        position: absolute;
+        right: 0px;
+        width: 100px;
+        height: 100%;
+        box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+        opacity: 1;
+        -webkit-transform: rotate(3deg) translate(0px, -4px);
+        -ms-transform: rotate(3deg) translate(0px, -4px);
+        transform: rotate(3deg) translate(0px, -4px);
+      }
+      #nprogress .spinner {
+        display: block;
+        position: fixed;
+        z-index: 1031;
+        top: 15px;
+        right: 15px;
+      }
+      #nprogress .spinner-icon {
+        width: 18px;
+        height: 18px;
+        box-sizing: border-box;
+        border: solid 2px transparent;
+        border-top-color: ${color};
+        border-left-color: ${color};
+        border-radius: 50%;
+        -webkit-animation: nprogresss-spinner 400ms linear infinite;
+        animation: nprogress-spinner 400ms linear infinite;
+      }
+      .nprogress-custom-parent {
+        overflow: hidden;
+        position: relative;
+      }
+      .nprogress-custom-parent #nprogress .spinner,
+      .nprogress-custom-parent #nprogress .bar {
+        position: absolute;
+      }
+      @-webkit-keyframes nprogress-spinner {
+        0% {
+          -webkit-transform: rotate(0deg);
+        }
+        100% {
+          -webkit-transform: rotate(360deg);
+        }
+      }
+      @keyframes nprogress-spinner {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    `}</style>
+	);
+}
