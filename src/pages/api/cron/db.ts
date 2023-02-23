51e6460aea8ce6345c051eb683556db812d9ba78
Matt DeKok <matt.dekok@gmail.com>
2023-02-22 20:24:03 -0600

    Vercel Cronjob

---
+++ src/pages/api/cron/db.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const result = await fetch(process.env.CRON_DB_URL || "");
+	response.status(200).json(await result.json());
+}
