9e5f70d942d5cd0accd9aa66468f5e1972a70c55
DeKok, Matt G <MGD3@na.valmont.com>
2023-05-26 21:38:36 -0500

    Export errors

--- src/pages/api/exports/characters/[characterId].ts
+++ src/pages/api/exports/characters/[characterId].ts
@@ -6,23 +6,28 @@ import type { NextApiHandler } from "next";
 import { getServerSession as getServerSession } from "next-auth";
 
 const handler: NextApiHandler = async function (req, res) {
-  const session = await getServerSession(req, res, nextAuthOptions);
+	const session = await getServerSession(req, res, nextAuthOptions);
 
-  if (!session || !session.user) return res.status(401).send("Unauthorized");
-  const { characterId } = req.query;
+	if (!session || !session.user) return res.status(401).send("Unauthorized");
+	const { characterId } = req.query;
 
-  try {
-    if (characterId === "all") {
-      const characters = await getAll(prisma, session.user.id);
-      return res.status(200).json(characters);
-    } else {
-      if (typeof characterId !== "string") return res.status(400).json({ message: "Invalid characterId" });
-      const character = await getOne(prisma, characterId);
-      return res.status(200).json(character);
-    }
-  } catch (err) {
-    return res.status(500).json({ error: parseError(err) });
-  }
+	try {
+		if (characterId === "all") {
+			const characters = await getAll(prisma, session.user.id);
+			return res.status(200).json(characters);
+		} else {
+			if (typeof characterId !== "string") return res.status(400).json({ message: "Invalid characterId" });
+
+			const character = await getOne(prisma, characterId);
+
+			if (!character) return res.status(404).json({ message: "Character not found" });
+			if (character.userId !== session.user.id) return res.status(401).json({ message: "Unauthorized" });
+
+			return res.status(200).json(character);
+		}
+	} catch (err) {
+		return res.status(500).json({ error: parseError(err) });
+	}
 };
 
 export default handler;
