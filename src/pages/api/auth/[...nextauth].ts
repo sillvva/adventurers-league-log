c3a03e38219f3f51f75cd92a5de988d2761bc72f
DeKok, Matt G <MGD3@na.valmont.com>
2023-05-27 11:21:29 -0500

    Bug Fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,30 +1,32 @@
 // Prisma adapter for NextAuth, optional and can be removed
-import { env } from '$src/env/server.mjs';
-import { prisma } from '$src/server/db/client';
-import NextAuth, { NextAuthOptions, type } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import { env } from "$src/env/server.mjs";
+import { prisma } from "$src/server/db/client";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import type { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    }
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET
-    })
-    // ...add more providers here
-  ]
+	// Include user.id on session
+	callbacks: {
+		session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			return session;
+		}
+	},
+	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET
+		})
+		// ...add more providers here
+	]
 };
 
 export default NextAuth(authOptions);
